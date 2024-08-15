970ad695eb538c765c604498e6675f0d5037ef8d
egor <egormalov.2007@gmail.com>
2024-08-15 18:12:23 +1000

    написан код для DemoTradingClient

--- WebService/src/main/java/com/cerberus/webservice/client/DemoTrading.java
+++ WebService/src/main/java/com/cerberus/webservice/client/DemoTrading.java
@@ -1,9 +1,17 @@
 package com.cerberus.webservice.client;
 
+import com.cerberus.webservice.dto.PortfolioItemDto;
 import com.cerberus.webservice.dto.UserBalanceDto;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface DemoTrading {
 
     Mono<UserBalanceDto> getBalance(Integer userId);
+
+    Mono<UserBalanceDto> createBalance(Integer userId);
+
+    Mono<UserBalanceDto> increaseBalance(Integer userId, Double sum);
+
+    Flux<PortfolioItemDto> getUserHistory(Integer userId);
 }
--- WebService/src/main/java/com/cerberus/webservice/client/impl/DemoTradingImpl.java
+++ WebService/src/main/java/com/cerberus/webservice/client/impl/DemoTradingImpl.java
@@ -1,10 +1,13 @@
 package com.cerberus.webservice.client.impl;
 
 import com.cerberus.webservice.client.DemoTrading;
+import com.cerberus.webservice.dto.PortfolioItemDto;
 import com.cerberus.webservice.dto.UserBalanceDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -12,9 +15,39 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class DemoTradingImpl implements DemoTrading {
 
+    private final WebClient webClient = WebClient.builder()
+            .baseUrl("http://localhost:5050/api/v1/trade")
+            .build();
 
     @Override
     public Mono<UserBalanceDto> getBalance(Integer userId) {
-        return null;
+        return this.webClient.get()
+                .uri("/user/{userId}", userId)
+                .retrieve()
+                .bodyToMono(UserBalanceDto.class);
+    }
+
+    @Override
+    public Mono<UserBalanceDto> createBalance(Integer userId) {
+        return this.webClient.post()
+                .uri("/user/{userId}", userId)
+                .retrieve()
+                .bodyToMono(UserBalanceDto.class);
+    }
+
+    @Override
+    public Mono<UserBalanceDto> increaseBalance(Integer userId, Double sum) {
+        return this.webClient.post()
+                .uri("/user/{userId}/increase?sum={sum}", userId, sum)
+                .retrieve()
+                .bodyToMono(UserBalanceDto.class);
+    }
+
+    @Override
+    public Flux<PortfolioItemDto> getUserHistory(Integer userId) {
+        return this.webClient.get()
+                .uri("/user/{userId}/history", userId)
+                .retrieve()
+                .bodyToFlux(PortfolioItemDto.class);
     }
 }
