60f2c2639c08ca2a648ea4cce906e1d3b5d8d8f6
egor <egormalov.2007@gmail.com>
2024-09-09 19:49:19 +1000

    к предыдущему комиту

---
+++ DemoTradingWeb/src/main/java/com/cerberus/demotradingweb/DemoTradingWebApplication.java
@@ -0,0 +1,13 @@
+package com.cerberus.demotradingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTradingWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTradingWebApplication.class, args);
+    }
+
+}
